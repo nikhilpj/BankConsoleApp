daf2993dcad7e54885ecaa993ff3383ea8ef3595
nikhilpj <nikhilpj98@gmail.com>
2025-01-30 22:55:47 +0530

    initial commit , created BankAccount class

---
+++ BankAccount.cs
@@ -0,0 +1,18 @@
+
+public class BankAccount
+{
+    public int BankAccountId { get; }
+    public string PersonName { get; set; }
+    public decimal Balance { get; }
+   
+    public void MakeWithDrawel(decimal amount, DateTime date, string note)
+    {
+
+    }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+
+    }
+
+}
